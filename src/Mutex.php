3f7e41366162f5bac910c34c6ae9a80608e4caa2
Cees-Jan Kiewiet <ceesjank@gmail.com>
2021-08-14 17:30:23 +0200

    Add Spin(lock)

--- src/Mutex.php
+++ src/Mutex.php
@@ -9,7 +9,6 @@ use RTCKit\React\Redlock\Custodian;
 use RTCKit\React\Redlock\Lock as RedisLock;
 use WyriHaximus\React\Mutex\Contracts\LockInterface;
 use WyriHaximus\React\Mutex\Contracts\MutexInterface;
-use WyriHaximus\React\Mutex\Lock;
 
 use const WyriHaximus\Constants\Numeric\ONE_FLOAT;
 
@@ -25,6 +24,13 @@ final class Mutex implements MutexInterface
     public function acquire(string $key, float $ttl): PromiseInterface
     {
         return $this->custodian->acquire($key, $ttl)->then(
+            static fn (?RedisLock $redisLock): ?Lock => $redisLock instanceof RedisLock ? new Lock($redisLock->getResource(), $redisLock->getToken()) : null
+        );
+    }
+
+    public function spin(string $key, float $ttl, int $attempts, float $interval): PromiseInterface
+    {
+        return $this->custodian->spin($attempts, $interval, $key, $ttl)->then(
             static fn (?RedisLock $redisLock): ?Lock => $redisLock instanceof RedisLock ? new Lock($redisLock->getResource(), $redisLock->getToken()) : null
         );
     }
