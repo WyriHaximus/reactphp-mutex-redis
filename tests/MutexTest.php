017ed55df8fa8497437a718cdad589b49dd31b36
Cees-Jan Kiewiet <ceesjank@gmail.com>
2021-09-01 01:04:59 +0200

    clue/redis-react v2.5

--- tests/MutexTest.php
+++ tests/MutexTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace WyriHaximus\Tests\React\Redis\Mutex;
 
 use Clue\React\Redis\Factory;
-use React\EventLoop\Loop;
 use RTCKit\React\Redlock\Custodian;
 use WyriHaximus\React\Mutex\AbstractMutexTestCase;
 use WyriHaximus\React\Mutex\Contracts\MutexInterface;
@@ -18,6 +17,6 @@ final class MutexTest extends AbstractMutexTestCase
     public function provideMutex(): MutexInterface
     {
         /** @phpstan-ignore-next-line */
-        return new Mutex(new Custodian((new Factory(Loop::get()))->createLazyClient(getenv('REDIS_DSN'))));
+        return new Mutex(new Custodian((new Factory())->createLazyClient(getenv('REDIS_DSN'))));
     }
 }
